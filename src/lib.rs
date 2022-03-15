2ec121a6bb72dda358c475b926307ec2cf7314b8
Finn Bear <finnbearone@gmail.com>
2022-03-15 21:40:01 +0000

    Fix.

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,16 @@ fn index() -> Html<&'static str> {
 <head>
 <title>Rustrict on Shuttle</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body style="padding: 1em;">
+<h1>Rustrict Shuttle Api</h1>
+<a href="https://crates.io/crates/rustrict">Rustrict</a> + <a href="https://www.shuttle.rs/">Shuttle</a> = <a href="https://github.com/finnbear/rustrict-shuttle">Rustrict Shuttle</a>
+<h2>Input</h2>
+<input id="input" class="form-control" oninput="censor()" type="text"/>
+<h2>Analysis</h2>
+<p id="analysis"></p>
+<h2>Output</h2>
+<textarea id="output" class="form-control" rows="10" readonly></textarea>
 <script>
 async function censor() {
 	const req = {text: document.getElementById("input").value};
@@ -33,16 +43,6 @@ async function censor() {
 }
 censor();
 </script>
-</head>
-<body style="padding: 1em;">
-<h1>Rustrict Shuttle Api</h1>
-<a href="https://crates.io/crates/rustrict">Rustrict</a> + <a href="https://www.shuttle.rs/">Shuttle</a> = <a href="https://github.com/finnbear/rustrict-shuttle">Rustrict Shuttle</a>
-<h2>Input</h2>
-<input id="input" class="form-control" oninput="censor()" type="text"/>
-<h2>Analysis</h2>
-<p id="analysis"></p>
-<h2>Output</h2>
-<textarea id="output" class="form-control" rows="10" readonly></textarea>
 </body>
 "###,
     )
