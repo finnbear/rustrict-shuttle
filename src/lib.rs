bdbfb628354f14b3b521f9bc352eb5676fc4830d
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-03-16 17:02:47 +0400

    Even Better HTML.

--- src/lib.rs
+++ src/lib.rs
@@ -10,42 +10,7 @@ use serde::{Deserialize, Serialize};
 
 #[get("/")]
 fn index() -> Html<&'static str> {
-    Html(
-        r###"
-<!DOCTYPE html>
-<head>
-<title>Rustrict on Shuttle</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-</head>
-<body style="padding: 1em;">
-<h1>Rustrict Shuttle Api</h1>
-<a href="https://crates.io/crates/rustrict">Rustrict</a> + <a href="https://www.shuttle.rs/">Shuttle</a> = <a href="https://github.com/finnbear/rustrict-shuttle">Rustrict Shuttle</a>
-<h2>Input</h2>
-<input id="input" class="form-control" oninput="censor()" type="text"/>
-<h2>Analysis</h2>
-<p id="analysis"></p>
-<h2>Output</h2>
-<textarea id="output" class="form-control" rows="10" readonly></textarea>
-<script>
-async function censor() {
-	const req = {text: document.getElementById("input").value};
-
-	let response = await fetch("/", {
-		method: "POST",
-		body: JSON.stringify(req),
-		headers: {
-			"Content-Type": "application/json"
-		}
-	});
-	let resp = await response.json();
-        document.getElementById("analysis").innerHTML = `${resp.analysis} (width = ${resp.width})`;
-	document.getElementById("output").innerHTML = resp.censored;
-}
-censor();
-</script>
-</body>
-"###,
-    )
+    Html(include_str!("index.html"))
 }
 
 #[derive(Default, Deserialize)]
